775796aab252f64b2b00eab25ab01f3ed3ec4894
汪志亮 <wangzhiliang@lanyidai.cn>
2019-01-09 19:19:57 +0800

    更新地址

--- src/api/env.js
+++ src/api/env.js
@@ -8,7 +8,7 @@
 let baseUrl = ''
 
 if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:8080'
+  baseUrl = 'http://chirowong.jios.org:9000'
 } else if (process.env.NODE_ENV === 'production') {
   baseUrl = 'http://chirowong.jios.org:9000'
 }
